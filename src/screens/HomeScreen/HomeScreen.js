b1c307c8e58177a77e849fded2b39c1484be4c99
Edina Biro <biro.edina.anita@student.ms.sapientia.ro>
2025-03-14 09:13:00 +0200

    Implementing Google Mpas while working out

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,22 +1,24 @@
-import { StyleSheet, Text, View, Dimensions, Button, TouchableOpacity} from 'react-native'
+import { StyleSheet, Text, View, Dimensions, Button, TouchableOpacity, ActivityIndicator} from 'react-native'
 import React, {useState, useEffect} from 'react';
 import MapView, {Marker} from 'react-native-maps';
 import * as Location from 'expo-location';
+import { useNavigation} from '@react-navigation/native';
+
 const HomeScreen = () => {
 
-  const [mapRegion, setMapRegion] = useState({
-    latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  });
+  const [mapRegion, setMapRegion] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  const navigation = useNavigation();
 
   const userLocaion = async () => {
+    setLoading(true);
     let {status} = await Location.requestForegroundPermissionsAsync();
     if( status !== 'granted')
     {
       setErrorMsg('Permission to access location was denied');
+      setLoading(false);
       return;
     }
     let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
@@ -26,28 +28,44 @@ const HomeScreen = () => {
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
     });
+    setLoading(false);
 
     console.log(location.coords.longitude,location.coords.latitude);
   }
+
   useEffect( () => {
     userLocaion();
   }, []);
+
   return (
     <View style = { styles.container}>
-      <MapView style={styles.map} 
-        region = {mapRegion}
+      {loading ? (
+          <ActivityIndicator size="large" color="#0000ff"/>
+      ): errorMsg ? (
+        <Text style={styles.errorText}>{errorMsg}</Text>
+      ): (
+        <>
+         <MapView style={styles.map} 
+        region = {mapRegion} onRegionChangeComplete={(region) => setMapRegion(region)}
         >
 
           <Marker coordinate={mapRegion} title = 'Marker'/>
       </MapView>
-      <Button title='Get Location' onPress={userLocaion} />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={userLocaion}>
+            <Text style={styles.buttonText}>Get Location</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={ () => navigation.navigate('WorkoutScreen', {initialLocation: {latitude: mapRegion.latitude, longitude: mapRegion.longitude}})}>
+            <Text style={styles.buttonText}>Workout</Text>
+        </TouchableOpacity>
+      </View>
+      </>
+      )}
+    </View> 
+  );
+};
 
 
-    </View>
-  )
-}
-
-export default HomeScreen
 
 const styles = StyleSheet.create({
   container: {
@@ -57,4 +75,31 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%'
   },
+  errorText: {
+    color: 'red',
+    fontSize: 16,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 100,
+    left: 10,
+    right:10,
+    flexDirection: 'column',
+    paddingVertical: 20,
+  },
+  button:{
+    marginVertical: 10,
+    backgroundColor: "#6200ee",
+    borderRadius: 20,
+    width: '50%',
+    padding: 10,
+    
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: '#fff',
+  }
 });
+
+export default HomeScreen;
--- src/screens/Workout/StartWorkoutScreen/StartWorkoutScreen.js
+++ src/screens/Workout/StartWorkoutScreen/StartWorkoutScreen.js
@@ -1,45 +1,68 @@
-import { StyleSheet, Text, View , Button, Platform, PermissionsAndroid, Animated} from 'react-native';
+import { StyleSheet, Text, View , Button, Platform, PermissionsAndroid, Animated, Alert, TouchableOpacity} from 'react-native';
 import React, { useState, useEffect, useRef} from 'react';
 import { useNavigation, useRoute} from '@react-navigation/native';
 import Geolocation from 'react-native-geolocation-service';
+import MapView, {Polyline} from 'react-native-maps';
+import { isDynamicPart } from 'expo-router/build/fork/getPathFromState-forks';
+import { ScrollView } from 'react-native-gesture-handler';
+import  Modal from 'react-native-modal';
 
 
 const StartWorkoutScreen = () => {
 
     const [countDown, setCountdown] = useState(3);
-    const [location, setLocation] = useState(null);
     const [distance, setDistance] = useState(0);
     const [calories, setCalories] = useState(0);
     const [pace, setPace] = useState(0);
+    const [duration, setDuration] = useState(0);
+    const [isWorkoutActive, setIsWorkoutActive] = useState(false);
+    const [isModalVisible, setIsModalVisible] = useState(false);
     const navigation= useNavigation();
     const route = useRoute();
     const {workoutName} = route.params;
-
+    const initialLocation = route.params?.initialLocation;
+    const [location, setLocation] = useState(initialLocation ?? null);
+    const [locationHistory, setLocationHistory] = useState(initialLocation ? [initialLocation] : []);
+    const [isPaused, setIsPaused] = useState(false);
     const fadeAnim = useRef(new Animated.Value(1)).current;
     const scaleAnim = useRef(new Animated.Value(1)).current;
+
+    useEffect(() => {
+        if(!location){
+            Geolocation.getCurrentPosition(
+                (position) => {
+                    setLocation(position.coords);
+                    setLocationHistory([position.coords]);
+                },
+                (error) => console.log(error),
+                {enableHighAccuracy: true}
+            );
+           
+        }
+    }, []);
 
     useEffect( () =>{
         if(countDown > 0){
             const timer = setTimeout( () => {
                 setCountdown(countDown -1);
                 animateCountdown();
-                //fadeIn();
             },1000);
             return () => clearTimeout(timer); 
         }else{
-            console.log('Workout started');
             startWorkout();
         }
     }, [countDown]);
 
-    const fadeIn = () => {
-        fadeAnim.setValue(0);
-        Animated.timing(fadeAnim, {
-            toValue:1,
-            duration: 500,
-            useNativeDriver: true,
-        }).start();
-    }
+    useEffect(() => {
+        let interval;
+        if(isWorkoutActive && !isPaused){
+            interval = setInterval(() => {
+                setDuration(prevDuration => prevDuration +1); 
+            }, 1000);
+        }
+        return () => clearInterval(interval);
+    }, [isWorkoutActive, isPaused]);
+
 
     const animateCountdown = () => {
         fadeAnim.setValue(0);
@@ -59,6 +82,7 @@ const StartWorkoutScreen = () => {
     };
 
     const requestLocationPermission = async () => {
+    try{
         if(Platform.OS === 'android') {
             const granted = await PermissionsAndroid.request(
                 PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
@@ -70,10 +94,12 @@ const StartWorkoutScreen = () => {
                     buttonPositive: 'OK',
                 }
             );
-            return garnted === PermissionsAndroid.RESULTS.GRANTED;
-        }else{
-            return true;
+            return granted === PermissionsAndroid.RESULTS.GRANTED;}
+        }catch(err){
+            console.warn(err);
+            return false;
         }
+        
     };
 
     const startWorkout = async () => {
@@ -84,15 +110,25 @@ const StartWorkoutScreen = () => {
             return;
         }
 
+        setIsWorkoutActive(true);
+
         Geolocation.watchPosition(
             (newLocation) => {
-                if(location) {
-                    const distanceIncrement = calculateDistance(location.coords, newLocation.coords);
-                    setDistance(distance + distanceIncrement);
-                    setPace(calculatePace(distance + distanceIncrement));
-                }
-                setLocation(newLocation);
-            },
+                setLocation((prevLocation) => {
+                    if(prevLocation){
+                        const distanceIncrement = calculateDistance(prevLocation, newLocation.coords);
+                        setDistance((prevDistance) => prevDistance + distanceIncrement);
+                        setPace(calculatePace(distance + distanceIncrement));
+                        setCalories(calculateCalories(distance + distanceIncrement));
+                    }
+                    return newLocation?.coords ?? prevLocation;
+                });
+
+                setLocationHistory((prev) => [...prev, newLocation.coords]);
+              
+                    
+                },
+            
             (error) => {
                 console.log(error);
             },
@@ -126,7 +162,7 @@ const StartWorkoutScreen = () => {
     };
 
     const calculatePace = (distance) => {
-        return distance > 0 ? (distance / (countDown / 60)).toFixed(2) : 0;
+        return distance > 0  && distance > 0 ? ((duration / distance) / 60).toFixed(2) : 0;
     };
 
     const calculateCalories = (distance) => {
@@ -135,10 +171,34 @@ const StartWorkoutScreen = () => {
         return (distance * caloriesPerKm).toFixed(2);
     };
 
+    const handlePause = () => {
+        setIsPaused(!isPaused);
+    };
 
+    const handleEndWorkout = () => {
+       setIsModalVisible(true);
+    };
+       
+
+    const confirmEndWorkout = () => {
+        setIsModalVisible(false);
+        navigation.navigate('HomeScreen');
+    };
+
+    const cancelEndWrokout = () => {
+        setIsModalVisible(false);
+
+    };
+
+    const formatTime = (seconds) => {
+        const minutes = Math.floor(seconds/60);
+        const remainingSeconds = seconds % 60;
+        return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+    }
 
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.scrollViewContent}>
+    
         {countDown > 0 ? (
             <Animated.Text 
                 style={[
@@ -150,15 +210,60 @@ const StartWorkoutScreen = () => {
                 </Animated.Text>
     
         ) : (
-            <View>
-            <Text style={styles.workoutText}>Workout Started: {workoutName}</Text>
-            <Text style={styles.detailsText}>Distance : {distance.toFixed(2)} km</Text>
-            <Text style={styles.detailsText}>Pace : {pace} min/km</Text>
-            <Text style={styles.detailsText}>Calories : {calories} kcal</Text>
-
+            <>
+            <View style={styles.mapContainer}>
+                <MapView style={styles.map}
+                    initialRegion={{
+                        latitude: location?.latitude ??  37.78825,
+                        longitude: location?.longitude ?? -122.4324,
+                        latitudeDelta: 0.01,
+                        longitudeDelta: 0.01,
+                    }} >
+                        <Polyline
+                            coordinates={locationHistory}
+                            strokeColor='#6200ee'
+                            strokeWidth={3}/>
+                    </MapView>
+            <View style={styles.infoContainer}>
+            <Text style={styles.workoutText}>🏋️‍♂️ Workout Started: {workoutName}</Text>
+            <View style={styles.detailRow}>
+            <Text style={styles.detailsText}>⏱ Duration: {formatTime(duration)} </Text>
+            <Text style={styles.detailsText}>🏃‍♂️ Distance : {distance.toFixed(2)} km</Text>
+            <Text style={styles.detailsText}>⏱ Pace : {pace} min/km</Text>
+            <Text style={styles.detailsText}>🔥Calories : {calories} kcal</Text>
             </View>
+
+
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button} onPress={handlePause}>
+                    <Text style={styles.buttonText}>{isPaused ? "Resume" : "Pause" }</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.button, styles.endButton]} onPress={handleEndWorkout}>
+                    <Text style={styles.buttonText}>End Workout</Text>
+                </TouchableOpacity>
+
+                <Modal isVisible={isModalVisible} onBackdropPress ={cancelEndWrokout}>
+                    <View style={styles.modalContent}>
+                        <Text style={styles.modalTitle}>Confirm End Of Workout</Text>
+                        <Text style={styles.modalMessage}>Are you sure you want to end your workout?</Text>
+                        <View style={styles.modalButtons}>
+                            <TouchableOpacity style={styles.modalCancelButton} onPress={cancelEndWrokout}>
+                                <Text style={styles.modalButtonText}>Cancel</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.modalConfirmButton} onPress={confirmEndWorkout}>
+                                <Text style={styles.modalButtonText}>OK</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                    </View>
+                </Modal>
+            </View>
+        </View>
+        </View>
+        </>
         )}
-    </View>
+
+    </ScrollView>
   );
 };
 
@@ -169,7 +274,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#fff',
+        
     },
 
     countdown: {
@@ -184,6 +289,101 @@ const styles = StyleSheet.create({
         fontSize: 18,
         marginTop: 10,
     },
-})
+    mapContainer: {
+        width: '100%',
+        height: '80%',
+        flex: 1,
+    },
+    infoContainer: {
+        padding: 20,
+        alignItems: 'center',
+        borderRadius: 15,
+        backgroundColor: 'rgba(255, 255, 255, 0.9)',
+        marginBottom: 20,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 5,
+        elevation: 5,
+    },
+    map: {
+        width: '100%',
+        height: '50%',
+        borderWidth: 2,
+        borderRadius: 15,
+        marginBottom: 20,
+    },
+    buttonContainer:{
+        flexDirection: 'row',
+        marginTop: 20,
+        justifyContent: 'space-between',
+        width: '100%',
+    },
+    button: {
+        backgroundColor: '#6200ee',
+        padding: 10,
+        borderRadius: 10,
+        width: '45%',
+        alignItems: 'center',
+    },
+    endButton: {
+        backgroundColor: "#007AFF",
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    scrollViewContent: {
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalContent:{
+        backgroundColor: '#fff',
+        padding: 22,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        borderColor: 'rgba(0, 0, 0, 0.1)',
+    },
+    modalTitle:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 15,
+    },
+    modalMessage: {
+        fontSize: 16,
+        marginBottom: 20,
+    },
+    modalButtons: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '100%',
+    },
+    modalCancelButton: {
+        backgroundColor: "#ccc",
+        padding: 12,
+        borderRadius: 8,
+        width: '40%',
+        alignItems: 'center',
+    },
+    modalButtonText: {
+        color: "#fff",
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    modalConfirmButton: {
+        backgroundColor: '#6200ee',
+        padding: 12,
+        borderRadius: 8,
+        width: '40%',
+        alignItems:'center',
+    }
+
+});
 
 export default StartWorkoutScreen;
