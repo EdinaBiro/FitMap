65b4a7741454f9c9a9e038da7c7a6d037f8e1915
Edina Biro <biro.edina.anita@student.ms.sapientia.ro>
2025-03-27 10:13:22 +0200

    Adding Onboarding screen to the app

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, StyleSheet, Text, useColorScheme, View , ScrollView} from 'react-native';
+import { SafeAreaView, StyleSheet, Text, useColorScheme, View , ScrollView, ActivityIndicator} from 'react-native';
 import React, {useState, useEffect, createContext} from 'react';
 import Navigation from './src/navigation/index';
 import { AppRegistry } from 'react-native';
@@ -9,27 +9,79 @@ import DrawerNavigation from './src/drawerNav/DrawerNavigation';
 import { ThemeProvider } from './src/drawerNav/ThemeContext';
 import { navigationRef } from './src/navigation/NavigationRef';
 import auth from '@react-native-firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+//import OnBoardingScreen from './src/screens/OnBoardingScreen';
 
- export const AuthContext = createContext();
+export const AuthContext = createContext();
 const App = () => {
 
   const [user, setUser] = useState(null);
+ // const [isFirstLaunch, setIsFirstLaunch] = useState(null);
+  //const [isLoading, setIsLoading] = useState(true);
 
-    function onAuthStateChanged(user) {
+  function onAuthStateChanged(user) {
     setUser(user);
   }
 
   useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
-  }, []);
+  //   const chechFirstLaunch = async () => {
+  //     try{
+  //       await AsyncStorage.removeItem('alreadyLaunched');
+  //       const value = await AsyncStorage.getItem('alreadyLaunched');
+  //       if(value === null){
+  //         await AsyncStorage.setItem('alreadyLaunched', 'true');
+  //         setIsFirstLaunch(true);
+  //       }else{
+  //         setIsFirstLaunch(false);
+  //       }
+  //     }catch(error){
+  //       console.error('Error checking first launch: ', error);
+  //       setIsFirstLaunch(false);
+  //     }
+  //   };
+
+    const authSubsrciber = auth().onAuthStateChanged(user => {
+      setUser(user);
+      // if(isFirstLaunch !== null){
+      //   setIsLoading(false);
+      // }
+    });
+
+    //chechFirstLaunch();
+
+    return () => {
+      authSubsrciber();
+    };
+  },[]);
+  //};
+  //},[isFirstLaunch]);
+
+ 
+
+  // if(isLoading){
+  //   return(
+  //     <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+  //       <ActivityIndicator size="large"/>
+  //     </View>
+  //   );
+  // }
 
   return (
      <AuthContext.Provider value={{ user }}>
          
         <ThemeProvider>
           <NavigationContainer ref={navigationRef}>
-            {user ? <DrawerNavigation/> : <Navigation />}
+            {user ? (
+              <DrawerNavigation/>
+             ) : (
+              <>
+              {/* {isFirstLaunch ? (
+                <OnBoardingScreen/>
+              ):( */}
+                <Navigation />
+             {/* )} */}
+              </>
+             )}
           </NavigationContainer>
         </ThemeProvider>
         </AuthContext.Provider>
