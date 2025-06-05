16fa9a2d44eee4757cd5bb9ca323629df19f94f5
EdinaBiro <edinab265@gmail.com>
2025-06-05 09:05:16 +0300

    took out the social part

---
+++ src/screens/PersonalPlanScreen/PersonalPlanScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const PersonalPlanScreen = () => {
+  return (
+    <View>
+      <Text>PersonalPlanScreen</Text>
+    </View>
+  );
+};
+
+export default PersonalPlanScreen;
